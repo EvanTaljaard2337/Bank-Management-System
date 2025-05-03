f3d6b9fa44d0248b597c25e7a7bd0cfd5cfe471a
EvanTaljaardPersonal <evantallies317@gmail.com>
2025-05-03 19:58:25 +0200

    Create an account functionality added.

--- src/java/za/ac/tut/web/checkBalanceServlet.java
+++ src/java/za/ac/tut/web/checkBalanceServlet.java
@@ -6,6 +6,7 @@ package za.ac.tut.web;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.List;
 import javax.ejb.EJB;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -14,7 +15,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import za.ac.tut.ejb.bl.BmAccountFacadeLocal;
+import za.ac.tut.ejb.bl.BmCustomerFacadeLocal;
 import za.ac.tut.entities.BmAccount;
+import za.ac.tut.entities.BmCustomer;
 
 /**
  *
@@ -23,21 +26,19 @@ import za.ac.tut.entities.BmAccount;
 public class checkBalanceServlet extends HttpServlet {
     @EJB
     private BmAccountFacadeLocal bma;
+    @EJB
+    private BmCustomerFacadeLocal cfl;
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         HttpSession session = request.getSession();
         Integer id = Integer.parseInt(request.getParameter("customerId"));
         
-        BmAccount account = getBalance(id);
-        session.setAttribute("acountDetails",account);
+        BmCustomer customer = cfl.find(id);
+        List<BmAccount> account = bma.findAccountsByCustId(customer);
         
+        session.setAttribute("acountDetails",account);
         RequestDispatcher disp = request.getRequestDispatcher("account_balance.jsp");
         disp.forward(request, response);
-    }
-    private BmAccount getBalance(Integer id){
-        BmAccount b = bma.find(id);
-        b.getBBalance();
-        return b;
     }
 }
